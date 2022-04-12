be6b16106ceb3bbde7f9f3eab1ea0aea9935f8dc
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-04-11 23:52:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quizz
+ JS-Project-7
---
+++ js/script.js
@@ -0,0 +1,184 @@
+// declação
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0;
+
+// perguntas
+const questions = [
+    {
+        "question": "PHP foi desenvolvido para qual fim?",
+        "answers": [
+            {
+                "answer": "back-end",
+                "correct": true
+            },
+            {
+                "answer": "front-end",
+                "correct": false
+            },
+            {
+                "answer": "Sisema operacional",
+                "correct": false
+            },
+            {
+                "answer": "Banco de dados",
+                "correct": false
+            },
+        ]
+    },
+    {
+        "question": "Uma forma de declarar variável em JavaScript:",
+        "answers": [
+            {
+                "answer": "$var",
+                "correct": false
+            },
+            {
+                "answer": "var",
+                "correct": true
+            },
+            {
+                "answer": "@var",
+                "correct": false
+            },
+            {
+                "answer": ".let",
+                "correct": false
+            },
+        ]
+    },
+    {
+        "question": "Qual o seletor de id no CSS",
+        "answers": [
+            {
+                "answer": "#",
+                "correct": true
+            },
+            {
+                "answer": ".",
+                "correct": false
+            },
+            {
+                "answer": "@",
+                "correct": false
+            },
+            {
+                "answer": "/",
+                "correct": false
+            },
+        ]
+    },
+]
+
+//  substituição do quizz para a primeira pergunta
+function init() {
+    createQuestion(0);
+}
+
+// criar pergunta
+function createQuestion(i) {
+
+    const oldButtons = answersBox.querySelectorAll("button");
+    oldButtons.forEach(function(btn) {
+        btn.remove();
+    });
+
+    const questionText = question.querySelector("#question-text");
+    const questionNumber = question.querySelector("#question-number");
+
+    questionText.textContent = questions[i].question;
+    questionNumber.textContent = i + 1;
+
+    // inserir alternativas
+    questions[i].answers.forEach(function(answer, i) {
+        const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
+
+        const letterBtn = answerTemplate.querySelector(".btn-letter");
+        const answerText = answerTemplate.querySelector(".question-answer");
+
+        letterBtn.textContent = letters[i];
+        answerText.textContent = answer['answer'];
+
+        answerTemplate.setAttribute("correct-answer", answer["correct"]);
+        answerTemplate.classList.remove("hide");
+        answerTemplate.classList.remove("answer-template");
+
+        answersBox.appendChild(answerTemplate);
+
+        // evento de click
+        answerTemplate.addEventListener("click", function(){
+            checkAnswer(this);
+        });
+
+    });
+
+    actualQuestion++;
+
+}
+
+// verificar responsta
+function checkAnswer(btn) {
+    const buttons = answersBox.querySelectorAll("button");
+    
+    buttons.forEach(function(button){
+        if(button.getAttribute("correct-answer") === 'true') {
+            button.classList.add("correct-answer");
+            if(btn === button) {
+                points++;
+            }
+        } else {
+            button.classList.add("wrong-answer");
+        }
+    });
+
+    nextQuestion();
+}
+
+function nextQuestion() {
+    setTimeout(function() {
+        if(actualQuestion >= questions.length) {
+            showSuccessMessage();
+            return;
+
+        }
+        createQuestion(actualQuestion);
+
+    }, 1500);
+}
+
+function showSuccessMessage() {
+
+    hideOrShowQuizz();
+
+    const score = ((points / questions.length) * 100).toFixed(2);
+
+    const displayScore = document.querySelector("#display-score span");
+
+    displayScore.textContent = score.toString();
+
+    const correctAnswers = document.querySelector("#correct-answers");
+    correctAnswers.textContent = points;
+
+    const totalQuestions = document.querySelector("#questions-qty");
+    totalQuestions.textContent = questions.length;
+
+}
+
+function hideOrShowQuizz() {
+    quizzContainer.classList.toggle("hide");
+    scoreContainer.classList.toggle("hide");
+}
+
+const restartBtn = document.querySelector("#restart");
+restartBtn.addEventListener("click", function(){
+        actualQuestion = 0;
+        points = 0;
+        hideOrShowQuizz();
+        init();
+});
+
+init();
